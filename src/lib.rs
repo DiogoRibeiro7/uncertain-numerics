27932d9287fe8b5c6fe201eb319f74a8bf78a028
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-16 22:59:33 +0100

    feat: expose scalar kernel API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![doc = include_str!("../README.md")]
 
 mod error;
+mod kernel;
+mod kernel_error;
 mod posterior;
 
 pub use error::PosteriorError;
+pub use kernel::{RbfKernel, ScalarKernel};
+pub use kernel_error::KernelError;
 pub use posterior::ScalarNormalPosterior;
